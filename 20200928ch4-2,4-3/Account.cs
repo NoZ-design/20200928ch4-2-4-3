c15bb136d3addde23f707c3e7ef4b20c61bc734e
NoZ-design <68496572+NoZ-design@users.noreply.github.com>
2020-09-28 15:18:48 +0800

    20200928ch4-2 4-3

---
+++ 20200928ch4-2,4-3/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _20200928ch4_2_4_3
+{
+    class Account
+    {
+
+        private string name;
+        public void SetName(string accountName)
+        {
+            name = accountName;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+
+    }
+}
---
+++ 20200928ch4-2,4-3/AccountTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _20200928ch4_2_4_3
+{
+    class AccountTest
+    {
+        static void Main(string[] args)
+        {
+            Account myAccount = new Account();
+
+            Console.WriteLine($"Initial name is:{myAccount.GetName()}");
+
+            Console.Write($"Enter the name:");
+            string theName = Console.ReadLine();
+            myAccount.SetName(theName);
+
+            Console.WriteLine($"myAccount's name is:{myAccount.GetName()}");
+        }
+    }
+}
